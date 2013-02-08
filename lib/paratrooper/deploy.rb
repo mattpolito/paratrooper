9c67d3a12e88d092e0614aa7d76009b3ae807a46
Matt Polito and Taylor Mock <dev+mattpolito+tmock12@hashrocket.com>
2013-02-08 13:47:42 -0500

    deploy tag updates tag reference point

--- lib/paratrooper/deploy.rb
+++ lib/paratrooper/deploy.rb
@@ -12,7 +12,7 @@ module Paratrooper
       @formatter     = options[:formatter] || DefaultFormatter.new
       @heroku        = options[:heroku] || HerokuWrapper.new(app_name, options)
       @tag_name      = options[:tag]
-      @deploy_tag    = options[:deploy_tag]
+      @deploy_tag    = options[:deploy_tag] || 'master'
       @system_caller = options[:system_caller] || SystemCaller.new
     end
 
@@ -29,15 +29,14 @@ module Paratrooper
     def update_repo_tag
       unless tag_name.nil? || tag_name.empty?
         notify_screen("Updating Repo Tag: #{tag_name}")
-        system_call "git tag #{tag_name} -f"
+        system_call "git tag #{tag_name} #{deploy_tag} -f"
         system_call "git push origin #{tag_name}"
       end
     end
 
     def push_repo
-      reference = deploy_tag || 'master'
-      notify_screen("Pushing #{reference} to Heroku")
-      system_call "git push -f #{git_remote} #{reference}:master"
+      notify_screen("Pushing #{deploy_tag} to Heroku")
+      system_call "git push -f #{git_remote} #{deploy_tag}:master"
     end
 
     def run_migrations

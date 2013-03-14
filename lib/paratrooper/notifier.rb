fb981de522a6fbaf013d092f332e679f955fb87e
Brandon Farmer and Matt Polito <dev+bthesorceror+mattpolito@hashrocket.com>
2013-03-14 15:16:21 -0400

    Documentation around Notifier
    To inform Custom notifier creators which methods are available to
    override

--- lib/paratrooper/notifier.rb
+++ lib/paratrooper/notifier.rb
@@ -1,9 +1,19 @@
 module Paratrooper
+
+  # Public: Shell object with methods to be overridden by other notifiers
+  #
+  # All notifiers should inherit from this class
+  #
   class Notifier
     def notify(step_name, options = {})
       self.send(step_name, options)
     end
 
+    #
+    # To create your own notifier override the following methods.
+    #
+
+    def setup(options = {}); end
     def activate_maintenance_mode(options = {}); end
     def deactivate_maintenance_mode(options = {}); end
     def update_repo_tag(options = {}); end
@@ -11,7 +21,6 @@ module Paratrooper
     def run_migrations(options = {}); end
     def app_restart(options = {}); end
     def warm_instance(options = {}); end
-    def setup(options = {}); end
     def teardown(options = {}); end
   end
 end
